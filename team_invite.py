06e361eaa48ef48cfa430c9d2ce290f4dbcdeb24
Kavindu Mihiranga <kmvithanage48@gmail.com>
2024-04-04 16:48:30 +0530

    updates

--- team_invite.py
+++ team_invite.py
@@ -27,21 +27,18 @@ def login():
     wait.until(EC.visibility_of_element_located((By.XPATH, "//input[@placeholder='Password']"))).send_keys(
         "ceyDigital#00")
     wait.until(EC.visibility_of_element_located((By.XPATH, "//span[normalize-space()='Log in']"))).click()
-    time.sleep(10)
 
 
 def header_team_invite_btn():
     invite_btn = wait.until(EC.visibility_of_element_located((By.XPATH,
-                                                              "/html/body/worklenz-root/worklenz-layout/nz-spin/div/nz-layout/nz-header/worklenz-header/div[2]/div/div/ul/li[2]")))
+                                                              "//span[normalize-space()='Invite Member']")))
     invite_btn.click()
-    time.sleep(5)
 
 
 def add_team_member():
     click_email = wait.until(EC.visibility_of_element_located((By.XPATH,
                                                                "//div[@id='cdk-overlay-0']/div/div[2]/div/div/div[2]/nz-spin/div/form/nz-form-item/nz-form-control/div/div/nz-select/nz-select-top-control")))
     click_email.click()
-    time.sleep(1)
     enter_email = wait.until(EC.visibility_of_element_located((By.XPATH,
                                                                "//div[@id='cdk-overlay-0']/div/div[2]/div/div/div[2]/nz-spin/div/form/nz-form-item/nz-form-control/div/div/nz-select/nz-select-top-control/nz-select-search/input")))
     random_email = fake.email()
@@ -55,9 +52,9 @@ def add_team_member():
     click_access = wait.until(EC.visibility_of_element_located((By.XPATH,
                                                                 "//div[@id='cdk-overlay-0']/div/div[2]/div/div/div[2]/nz-spin/div/form/nz-form-item[2]/nz-form-control/div/div/nz-select/nz-select-top-control/nz-select-item")))
     click_access.click()
-    time.sleep(1)
     dropdown = wait.until(EC.visibility_of_element_located((By.TAG_NAME, "nz-option-container")))
-    selects = dropdown.find_elements(By.TAG_NAME, "nz-option-item")
+    dropdown_wait = WebDriverWait(dropdown, 10)
+    selects = dropdown_wait.until(EC.presence_of_all_elements_located((By.TAG_NAME, "nz-option-item")))
     no = random.randint(0, 1)
     selects[no].click()
     wait.until(EC.visibility_of_element_located((By.XPATH, "//span[normalize-space()='Add to team']"))).click()
@@ -65,7 +62,7 @@ def add_team_member():
 
 
 def go_to_team_members_list():
-    wait.until(EC.visibility_of_element_located((By.XPATH, "//li[5]"))).click()
+    wait.until(EC.presence_of_element_located((By.XPATH, "//li[5]"))).click()
     profile_details = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "profile-details-dropdown")))
     profile_details_wait = WebDriverWait(profile_details, 10)
     profile_details_wait.until(EC.presence_of_all_elements_located((By.TAG_NAME, "li")))[1].click()
