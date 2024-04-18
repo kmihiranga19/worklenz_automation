ed2654188df05a2b009a152f548c8e0ade962870
Kavindu Mihiranga <kmvithanage48@gmail.com>
2024-04-18 16:59:38 +0530

    updates

---
+++ locators.py
@@ -0,0 +1,15 @@
+class Xpath_locators:
+    email_xpath = "//input[@placeholder='Email']"
+    password_xpath = "//input[@placeholder='Password']"
+    login_btn_xpath = "//span[normalize-space()='Log in']"
+    navigation_bar_invite_btn_xpath = "//span[normalize-space()='Invite']"
+    member_invite_click_email_xpath = "//div[@id='cdk-overlay-0']/div/div[2]/div/div/div[2]/nz-spin/div/form/nz-form-item/nz-form-control/div/div/nz-select/nz-select-top-control"
+    member_invite_enter_email_xpath = "//div[@id='cdk-overlay-0']/div/div[2]/div/div/div[2]/nz-spin/div/form/nz-form-item/nz-form-control/div/div/nz-select/nz-select-top-control/nz-select-search/input"
+    member_invite_click_job_title_xpath = "//div[@id='cdk-overlay-0']/div/div[2]/div/div/div[2]/nz-spin/div/form/worklenz-job-titles-autocomplete/form/nz-form-item/nz-form-control/div/div/input"
+    member_invite_enter_job_title_xpath = "//div[@id='cdk-overlay-0']/div/div[2]/div/div/div[2]/nz-spin/div/form/worklenz-job-titles-autocomplete/form/nz-form-item/nz-form-control/div/div/input"
+    member_invite_click_access_xpath = "//div[@id='cdk-overlay-0']/div/div[2]/div/div/div[2]/nz-spin/div/form/nz-form-item[2]/nz-form-control/div/div/nz-select/nz-select-top-control/nz-select-item"
+    member_invite_add_team_xpath = "//span[normalize-space()='Add to team']"
+
+
+class Class_locators:
+    profile_details_class = "profile-details-dropdown"
--- team_invite.py
+++ team_invite.py
@@ -7,6 +7,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 from faker import Faker
+from locators import Xpath_locators, Class_locators
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
@@ -23,41 +24,42 @@ def main():
 
 
 def login():
-    wait.until(EC.visibility_of_element_located((By.XPATH, "//input[@placeholder='Email']"))).send_keys(
+    wait.until(EC.visibility_of_element_located((By.XPATH, Xpath_locators.email_xpath))).send_keys(
         "ncsdn@v.com")
-    wait.until(EC.visibility_of_element_located((By.XPATH, "//input[@placeholder='Password']"))).send_keys(
+    wait.until(EC.visibility_of_element_located((By.XPATH, Xpath_locators.password_xpath))).send_keys(
         "ceyDigital#00")
-    wait.until(EC.visibility_of_element_located((By.XPATH, "//span[normalize-space()='Log in']"))).click()
+    wait.until(EC.visibility_of_element_located((By.XPATH, Xpath_locators.login_btn_xpath))).click()
 
 
 def header_team_invite_btn():
-    invite_btn = wait.until(EC.visibility_of_element_located((By.XPATH, "//span[normalize-space()='Invite']")))
+    invite_btn = wait.until(
+        EC.visibility_of_element_located((By.XPATH, Xpath_locators.navigation_bar_invite_btn_xpath)))
     invite_btn.click()
 
 
 def add_team_member():
-    click_email = wait.until(EC.visibility_of_element_located((By.XPATH,
-                                                               "//div[@id='cdk-overlay-0']/div/div[2]/div/div/div[2]/nz-spin/div/form/nz-form-item/nz-form-control/div/div/nz-select/nz-select-top-control")))
+    click_email = wait.until(
+        EC.visibility_of_element_located((By.XPATH, Xpath_locators.member_invite_click_email_xpath)))
     click_email.click()
-    enter_email = wait.until(EC.visibility_of_element_located((By.XPATH,
-                                                               "//div[@id='cdk-overlay-0']/div/div[2]/div/div/div[2]/nz-spin/div/form/nz-form-item/nz-form-control/div/div/nz-select/nz-select-top-control/nz-select-search/input")))
+    enter_email = wait.until(
+        EC.visibility_of_element_located((By.XPATH, Xpath_locators.member_invite_enter_email_xpath)))
     random_email = fake.email()
     enter_email.send_keys(random_email, Keys.ENTER)
-    click_job_title = wait.until(EC.visibility_of_element_located((By.XPATH,
-                                                                   "//div[@id='cdk-overlay-0']/div/div[2]/div/div/div[2]/nz-spin/div/form/worklenz-job-titles-autocomplete/form/nz-form-item/nz-form-control/div/div/input")))
+    click_job_title = wait.until(
+        EC.visibility_of_element_located((By.XPATH, Xpath_locators.member_invite_click_job_title_xpath)))
     click_job_title.click()
-    enter_job_title = wait.until(EC.visibility_of_element_located((By.XPATH,
-                                                                   "//div[@id='cdk-overlay-0']/div/div[2]/div/div/div[2]/nz-spin/div/form/worklenz-job-titles-autocomplete/form/nz-form-item/nz-form-control/div/div/input")))
+    enter_job_title = wait.until(
+        EC.visibility_of_element_located((By.XPATH, Xpath_locators.member_invite_enter_job_title_xpath)))
     enter_job_title.send_keys("Software engineer", Keys.ENTER)
-    click_access = wait.until(EC.visibility_of_element_located((By.XPATH,
-                                                                "//div[@id='cdk-overlay-0']/div/div[2]/div/div/div[2]/nz-spin/div/form/nz-form-item[2]/nz-form-control/div/div/nz-select/nz-select-top-control/nz-select-item")))
+    click_access = wait.until(
+        EC.visibility_of_element_located((By.XPATH, Xpath_locators.member_invite_click_access_xpath)))
     click_access.click()
     dropdown = wait.until(EC.visibility_of_element_located((By.TAG_NAME, "nz-option-container")))
     dropdown_wait = WebDriverWait(dropdown, 10)
     selects = dropdown_wait.until(EC.visibility_of_all_elements_located((By.TAG_NAME, "nz-option-item")))
     no = random.randint(0, 1)
     selects[no].click()
-    wait.until(EC.visibility_of_element_located((By.XPATH, "//span[normalize-space()='Add to team']"))).click()
+    wait.until(EC.visibility_of_element_located((By.XPATH, Xpath_locators.member_invite_add_team_xpath))).click()
     time.sleep(6)
 
     return random_email
@@ -65,7 +67,8 @@ def add_team_member():
 
 def go_to_team_members_list():
     wait.until(EC.visibility_of_element_located((By.XPATH, "//li[5]"))).click()
-    profile_details = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "profile-details-dropdown")))
+    profile_details = wait.until(
+        EC.visibility_of_element_located((By.CLASS_NAME, Class_locators.profile_details_class)))
     profile_details_wait = WebDriverWait(profile_details, 10)
     profile_details_wait.until(EC.visibility_of_all_elements_located((By.TAG_NAME, "li")))[1].click()
     sider = wait.until(EC.visibility_of_element_located((By.TAG_NAME, "nz-sider")))
@@ -113,6 +116,11 @@ def check_invited_member_add_to_list(inv_member, members_list):
     for member_list in members_list:
         if inv_member == member_list:
             print(f"Successfully '{inv_member}' member add to team ")
+            break
+
+        else:
+            print(f"{inv_member} not add to team")
+            break
 
 
 main()
