a92f687768b8de604f379eea9339564423d4eb5e
kavinduCeydigital <71685791+kmihiranga19@users.noreply.github.com>
2024-06-20 15:39:40 +0530

    updates

--- login/test_login.py
+++ login/test_login.py
@@ -16,15 +16,17 @@ class Test_login:
         self.driver.maximize_window()
 
     def test_verify_able_login_with_credentials(self):
-        self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "input[placeholder='Email']"))).send_keys("ceyare4516@mliok.com")
-        self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "input[placeholder='Password']"))).send_keys("ceyDigital#00")
+        self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "input[placeholder='Email']"))).send_keys(
+            "ceyare4516@mliok.com")
+        self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "input[placeholder='Password']"))).send_keys(
+            "ceyDigital#00")
         self.wait.until(EC.visibility_of_element_located((By.XPATH, "//span[normalize-space()='Log in']"))).click()
 
         try:
             self.wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "logo-holder")))
 
         except NoSuchElementException:
-            pytest.fail("Test case fail: User can not login with correct credentials")
+            pytest.fail("Test case fail: Verify user unable login with correct credentials")
 
     def test_verify_unable_login_with_invalid_email(self):
         self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "input[placeholder='Email']"))).send_keys(
@@ -33,24 +35,36 @@ class Test_login:
             "ceyDigital#00")
         self.wait.until(EC.visibility_of_element_located((By.XPATH, "//span[normalize-space()='Log in']"))).click()
 
-        
+        try:
+            self.wait.until(
+                EC.visibility_of_element_located((By.XPATH, "//div[normalize-space()='Please input your email!']")))
 
+        except NoSuchElementException:
+            pytest.fail("Test case fail: Verify user able login with invalid email")
 
+    def test_verify_unable_login_with_invalid_password(self):
+        self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "input[placeholder='Email']"))).send_keys(
+            "ceyare4516@mliok")
+        self.wait.until(EC.visibility_of_element_located((By.XPATH, "//span[normalize-space()='Log in']"))).click()
 
+        try:
+            self.wait.until(
+                EC.visibility_of_element_located((By.XPATH, "//div[normalize-space()='Please input your Password!']")))
 
+        except NoSuchElementException:
+            pytest.fail("Test case fail: Verify user able login with invalid password")
 
+    def test_verify_unable_login_with_wrong_credentials(self):
+        self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "input[placeholder='Email']"))).send_keys(
+            "ceyare4516@mliok.com")
+        self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "input[placeholder='Password']"))).send_keys(
+            "ceyDigital")
+        self.wait.until(EC.visibility_of_element_located((By.XPATH, "//span[normalize-space()='Log in']"))).click()
 
-
-        
-
-
-
-
-
-
-
-
-
-
+        try:
+            self.wait.until(
+                EC.visibility_of_element_located((By.XPATH, "//div[normalize-space()='Login failed!']")))
+        except NoSuchElementException:
+            pytest.fail("Test case fail: Verify user able login with wrong password")
 
 
