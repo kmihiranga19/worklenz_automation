083fd3500f3e4c0f03da349b5517e21bac4fc57b
kavinduCeydigital <71685791+kmihiranga19@users.noreply.github.com>
2024-06-28 15:05:08 +0530

    updates

--- settings-change_password/test_settings-change_password.py
+++ settings-change_password/test_settings-change_password.py
@@ -1,5 +1,5 @@
 import time
-
+from faker import Faker
 from selenium import webdriver
 import pytest
 from selenium.common import NoSuchElementException
@@ -9,6 +9,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 class Test_change_password:
+
     def setup_method(self):
         options = webdriver.ChromeOptions()
         options.add_experimental_option("detach", True)
@@ -21,10 +22,10 @@ class Test_change_password:
 
     def login(self, email, password):
         self.wait.until(
-                EC.visibility_of_element_located((By.CSS_SELECTOR, "input[placeholder='Email']"))).send_keys(email)
+            EC.visibility_of_element_located((By.CSS_SELECTOR, "input[placeholder='Email']"))).send_keys(email)
         self.wait.until(
-                EC.visibility_of_element_located((By.CSS_SELECTOR, "input[placeholder='Password']"))).send_keys(
-                password)
+            EC.visibility_of_element_located((By.CSS_SELECTOR, "input[placeholder='Password']"))).send_keys(
+            password)
         self.wait.until(EC.visibility_of_element_located((By.XPATH, "//span[normalize-space()='Log in']"))).click()
 
         return password
@@ -34,31 +35,34 @@ class Test_change_password:
         time.sleep(5)
         self.driver.get("https://uat.app.worklenz.com/worklenz/settings/password")
         try:
-            self.wait.until(EC.visibility_of_element_located((By.XPATH, "//label[normalize-space()='Confirm New Password']")))
+            self.wait.until(
+                EC.visibility_of_element_located((By.XPATH, "//label[normalize-space()='Confirm New Password']")))
 
         except NoSuchElementException:
             pytest.fail("Test case fail: Verify change password page")
 
     def test_verify_user_able_change_password(self):
-        self.login("bsdcsd@v.com", "ceyDigital#00")
+        faker = Faker()
+        no = faker.number()
+        current_password = 'ceyDigital#03'
+        new_password = 'ceyDigital#' + no
+        print(new_password)
+        self.login("bsdcsd@v.com", current_password)
         time.sleep(5)
         self.driver.get("https://uat.app.worklenz.com/worklenz/settings/password")
 
-        self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "input[placeholder='Enter your current password']"))).send_keys("ceyDigital#00")
-        self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "input[placeholder='New Password']"))).send_keys("ceyDigital#01")
-        self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "input[placeholder='Confirm New Password']"))).send_keys("ceyDigital#01")
-
-        
-
-
-
-
-
-
-
-
-
-
-
-
+        self.wait.until(EC.visibility_of_element_located(
+            (By.CSS_SELECTOR, "input[placeholder='Enter your current password']"))).send_keys(current_password)
+        self.wait.until(
+            EC.visibility_of_element_located((By.CSS_SELECTOR, "input[placeholder='New Password']"))).send_keys(new_password)
+        self.wait.until(
+            EC.visibility_of_element_located((By.CSS_SELECTOR, "input[placeholder='Confirm New Password']"))).send_keys(
+            new_password)
+        self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "button[type='submit']"))).click()
+        current_password = new_password
+        wl_header = self.wait.until(EC.visibility_of_element_located((By.TAG_NAME, "worklenz-header")))
+        wl_header_wait = WebDriverWait(wl_header, 10)
+        left_header = wl_header_wait.until(EC.visibility_of_element_located((By.TAG_NAME, "ul")))[1]
+        left_header_wait = WebDriverWait(left_header, 10)
+        left_header_wait.until(EC.visibility_of_element_located((By.TAG_NAME, "li")))[-1].click()
 
